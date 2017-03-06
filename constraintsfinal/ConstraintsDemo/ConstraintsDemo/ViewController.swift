c22f08efcfa9c570d8f4351c7fe22cbc70cea594
Rajat Dhasmana <rajatdhasmana@Priyankas-MacBook-Air.local>
2017-03-06 18:27:45 +0530

    no message

---
+++ constraintsfinal/ConstraintsDemo/ConstraintsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConstraintsDemo
+//
+//  Created by Rajat Dhasmana on 06/03/17.
+//  Copyright © 2017 appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
